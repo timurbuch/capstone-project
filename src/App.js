cb8a510f698b539aab1ab4cd46b59d966b28cb9d
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-11 13:38:15 +0200

    Import PlayerList to App.js, provide mockData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import PlayerList from './components/PlayerList/PlayerList'
+const mockData = [
+  { name: 'player 1', id: 1 },
+  { name: 'player 2', id: 2 },
+  { name: 'player 3', id: 3 },
+  { name: 'player 4', id: 4 },
+  { name: 'player 5', id: 5 },
+  { name: 'player 6', id: 6 },
+]
+
 function App() {
   return (
     <div className="App">
-      <p>Capstone Project...LFG!!!</p>
+      <PlayerList data={mockData} />
     </div>
   )
 }
