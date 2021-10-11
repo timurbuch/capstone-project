a09e4609bf6d5e97815eb20d5cbe2e923087785d
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-11 15:29:43 +0200

    Remove App.spec.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders a playerListItem', () => {
-  render(<App />)
-  const listElement = screen.getByText('player 1')
-  expect(listElement).toBeInTheDocument()
-})
