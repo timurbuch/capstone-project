14a9b73d4d1351ff79dedcecd5758bff76b7d7a6
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-11 15:28:30 +0200

    Update ListItem.spec.js

--- src/components/ListItem/ListItem.spec.js
+++ src/components/ListItem/ListItem.spec.js
@@ -1,12 +1,17 @@
 import { render, screen } from '@testing-library/react'
 
 import ListItem from './ListItem'
-const testName = 'Test'
 
 describe('ListItem', () => {
+  const testName = 'Andre Agassi'
   it('renders', () => {
     render(<ListItem name={testName} />)
     const listItem = screen.getByRole('listitem')
     expect(listItem).toBeInTheDocument()
+  })
+  it('shows the name of a player', () => {
+    render(<ListItem name={testName} />)
+    const testname = screen.getByText('Andre Agassi')
+    expect(testname).toBeInTheDocument()
   })
 })
