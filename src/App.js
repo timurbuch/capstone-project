8f16772c5ce5e74f300df107dfedbd2d2cc9ee7f
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-12 10:07:38 +0200

    Move GlobalStyle import to index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import PlayerList from './components/PlayerList/PlayerList'
 import data from './data.json'
-import GlobalStyle from './GlobalStyles/GlobalStyles'
 
 const mockData = data.data
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <PlayerList data={mockData} />
-    </>
-  )
+  return <PlayerList data={mockData} />
 }
 
 export default App
