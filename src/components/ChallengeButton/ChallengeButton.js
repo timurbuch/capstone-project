1f020206e75be1914b3528c13c543683afd71089
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-13 16:54:08 +0200

    Create ChallengeButton component and test

---
+++ src/components/ChallengeButton/ChallengeButton.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro'
+
+const ChallengeButton = ({ name, isChallenged, toggleChallenge }) => {
+  const handleChallenge = () => {
+    toggleChallenge()
+  }
+
+  return (
+    <StyledChallengeButton
+      isChallenged={isChallenged}
+      onClick={handleChallenge}
+    >
+      {isChallenged ? 'Cancel Challenge' : `Challenge ${name}`}
+    </StyledChallengeButton>
+  )
+}
+
+const StyledChallengeButton = styled.button`
+  font-size: large;
+  border-radius: 1rem;
+  padding: 3px;
+  color: whitesmoke;
+  background-color: ${({ isChallenged }) =>
+    isChallenged ? '#A5413E' : 'blue'};
+`
+export default ChallengeButton
