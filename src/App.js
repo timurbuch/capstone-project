19ff51cb0fc16fc823449ce73e830035c0a6f74b
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-14 14:19:02 +0200

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import PlayerList from './components/PlayerList/PlayerList'
 import players from './players.json'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const mockData = players.players
 function App() {
-  return <PlayerList players={mockData} />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <PlayerList players={mockData} />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
