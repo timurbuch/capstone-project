75ef8c23b44c1edfbd7b53987c40d5cd847cef37
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-15 10:26:07 +0200

    Add actions to ChallengeButton stories

--- src/components/ChallengeButton/ChallengeButton.stories.js
+++ src/components/ChallengeButton/ChallengeButton.stories.js
@@ -1,5 +1,5 @@
 import ChallengeButton from './ChallengeButton'
-
+import { action } from '@storybook/addon-actions'
 export default {
   title: 'ChallengeButton',
   component: ChallengeButton,
@@ -8,12 +8,14 @@ const Template = args => <ChallengeButton {...args} />
 
 export const basicChallengeButton = Template.bind({})
 basicChallengeButton.args = {
+  toggleChallenge: action('challenge on'),
   name: 'John McEnroe',
   id: 123,
   isChallenged: false,
 }
 export const activeChallengeButton = Template.bind({})
 activeChallengeButton.args = {
+  toggleChallenge: action('challenge off'),
   name: 'John McEnroe',
   id: 123,
   isChallenged: true,
