2aca59e4e6913a2dd0073977bfa304b69076bbe7
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-18 10:30:02 +0200

    Create ResultForm component with test and story

---
+++ src/components/ResultForm/ResultForm.stories.js
@@ -0,0 +1,9 @@
+import ResultForm from './ResultForm'
+import { action } from '@storybook/addon-actions'
+export default {
+  title: 'ResultForm',
+  component: ResultForm,
+}
+const Template = () => <ResultForm />
+
+export const basicResultForm = Template.bind({})
