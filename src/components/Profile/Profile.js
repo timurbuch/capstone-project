32c436c1ce3c8995292ba7511dc7987774f8f0ec
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-18 16:58:10 +0200

    Create Profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/macro'
+
+const Profile = () => {
+  return (
+    <ProfileWrapper>
+      <h2>Your Profile</h2>
+      <AvatarWrapper>
+        <img src="https://picsum.photos/200" alt="user profile pic" />
+        <h3>Test User</h3>
+      </AvatarWrapper>
+      <p>Hier könnte Ihre Werbung stehen...</p>
+    </ProfileWrapper>
+  )
+}
+
+const ProfileWrapper = styled.section`
+  height: 88vh;
+  margin: 0 1rem;
+  background-color: #4758d6;
+  border-left: 1px solid white;
+  border-right: 1px solid white;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 2re;
+  align-items: center;
+`
+
+const AvatarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  img {
+    border-radius: 50%;
+  }
+`
+export default Profile
