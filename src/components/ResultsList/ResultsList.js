df66cf629cd98926048bddf60a09d2924d23f50c
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-18 12:20:13 +0200

    Create ResultsList placeholder / import in App.js

---
+++ src/components/ResultsList/ResultsList.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro'
+
+const ResultsList = () => {
+  return <ResultsPage>In the future you will see your results here</ResultsPage>
+}
+
+const ResultsPage = styled.div`
+  background-color: black;
+  height: 100vh;
+  font-size: large;
+  color: white;
+`
+export default ResultsList
