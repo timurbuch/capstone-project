733bdc8c03d8d93242d6992584c68f0105c4533a
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-20 15:09:51 +0200

    Delete unnecessary file

--- src/lib/setChallengeStatus.js
+++
@@ -1,6 +0,0 @@
-const setChallengeStatus = (players, name) => {
-  const challengedPlayer = players.find(player => player.name === name)
-  const status = challengedPlayer.challenged
-  challengedPlayer.challenged = !status
-}
-export default setChallengeStatus
