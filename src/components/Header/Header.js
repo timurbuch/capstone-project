6efe6d838d111bb24a711e3c0653b5e749163d5c
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-28 10:57:37 +0200

    Aplly additional styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,6 +17,7 @@ const HeaderWrapper = styled.header`
   top: 0px;
   margin: 0;
   z-index: 10;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 `
 const Logo = styled.h2`
   font-family: 'Nfl Tampa Bay Buccaneers';
@@ -26,7 +27,8 @@ const Logo = styled.h2`
   padding: 0.5rem 1rem;
   width: fit-content;
 `
-const Subsection = styled.h4`
+const Subsection = styled.div`
   font-size: 20px;
+  width: fit-content;
 `
 export default Header
