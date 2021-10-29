87bf77cdbfbdf9a6ce27d210c2e8bf084cd91e6f
Timur Buchberger <timo.buchberger@gmail.com>
2021-10-29 14:13:34 +0200

    Fix typo

--- src/components/LoginForm/LogInForm.spec.js
+++ src/components/LoginForm/LogInForm.spec.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import LoginForm from './LoginForm'
 
 describe('LoginForm', () => {
-  it('renders a form with an input fields', () => {
+  it('renders a form with an input field', () => {
     render(<LoginForm />)
     const nameInput = screen.getByPlaceholderText('Username')
     expect(nameInput).toBeInTheDocument()
